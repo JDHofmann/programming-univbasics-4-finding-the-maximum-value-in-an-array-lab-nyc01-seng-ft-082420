b4162bcb880bc946a4539ac72cfe68b34e36ddd5
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-11 22:37:23 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  # Add your solution here
+  i = 1  
+  current_max = array[0] 
+  while i < array.length do 
+    if array[i] > current_max
+      current_max = array[i]
+      
+    end 
+  end 
+  current_max
 end
